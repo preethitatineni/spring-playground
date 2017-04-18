c9e7493a9fe4093717e1e7e48675c7708b63905e
Preethi Tatineni <samantha.tatineni@gmail.com>
2017-04-17 21:26:51 -0600

    math pi endpoint and test case

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -13,4 +13,9 @@ public class HelloController {
         return "Hello from Spring!";
     }
 
+    @GetMapping("/math/pi")
+    public String piValue() {
+        return "3.141592653589793";
+    }
+
 }
