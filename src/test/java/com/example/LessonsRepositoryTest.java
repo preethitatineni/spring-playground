2a424031bbc614bff6cf242ad33fb9636c8d9a42
Preethi Tatineni <samantha.tatineni@gmail.com>
2017-05-07 12:52:11 -0600

    lesson repository tests

---
+++ src/test/java/com/example/LessonsRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.example;
+
+import com.example.model.Lesson;
+import com.example.repositories.LessonRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import javax.transaction.Transactional;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by trainer6 on 5/7/17.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LessonsRepositoryTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    LessonRepository repository;
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testCreate() throws Exception {
+        MockHttpServletRequestBuilder request = post("/lessons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\": \"lessonOne\"}");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", instanceOf(Number.class) ));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testLessonById() throws Exception {
+        Lesson lesson = new Lesson();
+        lesson.setTitle("TestLesson");
+        Lesson savedLesson = repository.save(lesson);
+
+        MockHttpServletRequestBuilder request = get("/lessons/"+savedLesson.getId())
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", equalTo(lesson.getId().intValue()) ));
+    }
+}
